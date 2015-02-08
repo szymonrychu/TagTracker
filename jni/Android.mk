f25e7421947b529b462dcb11a5155b91227b656a
Szymon Richert <szymon.rychu@gmail.com>
2015-02-09 00:50:51 +0100

    Import in Ubuntu

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,7 +4,7 @@ TOP_PATH := $(call my-dir)/..
 
 include $(CLEAR_VARS)
 OPENCV_INSTALL_MODULES:=on
-include ${OPENCV_HOME}\sdk\native\jni\OpenCV.mk
+include ${OPENCV_HOME}/sdk/native/jni/OpenCV.mk
 LOCAL_PATH=$(TOP_PATH)/jni
 LOCAL_MODULE    := recognizer
 LOCAL_SRC_FILES := recognizer.cpp
@@ -15,7 +15,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 OPENCV_INSTALL_MODULES:=on
-include ${OPENCV_HOME}\sdk\native\jni\OpenCV.mk
+include ${OPENCV_HOME}/sdk/native/jni/OpenCV.mk
 LOCAL_PATH=$(TOP_PATH)/jni
 LOCAL_MODULE    := calibrate
 LOCAL_SRC_FILES := calibrate.cpp
@@ -26,7 +26,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 OPENCV_INSTALL_MODULES:=on
-include ${OPENCV_HOME}\sdk\native\jni\OpenCV.mk
+include ${OPENCV_HOME}/sdk/native/jni/OpenCV.mk
 LOCAL_PATH=$(TOP_PATH)/jni
 LOCAL_MODULE    := misc
 LOCAL_SRC_FILES := misc.cpp
@@ -37,7 +37,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 OPENCV_INSTALL_MODULES:=on
-include ${OPENCV_HOME}\sdk\native\jni\OpenCV.mk
+include ${OPENCV_HOME}/sdk/native/jni/OpenCV.mk
 LOCAL_PATH=$(TOP_PATH)/jni
 LOCAL_MODULE    := detector
 LOCAL_SRC_FILES := detector.cpp
